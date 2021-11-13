943bb37daa741f1e2780c7f58c45d54b895070f5
구영모 <dudah754@naver.com>
2021-11-13 11:43:14 +0900

    var / val 변수 만드는 용법 차이 경험

--- app/src/main/java/com/nepplus/kotlingrammar_211113/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlingrammar_211113/MainActivity.kt
@@ -2,10 +2,31 @@ package com.nepplus.kotlingrammar_211113
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnVariable.setOnClickListener {
+
+ //            이 안에 적는 코드 - 변수 문법 연습 01
+
+            var myName : String
+
+            myName ="구영모"
+            myName = "김영모"
+
+            Log.d("문법연습01", myName)
+
+            val friendName : String
+            friendName = "김갑생할머니"
+//            friendName = "갑돌이" 여기는 주석이니까 아무말 적어도 됨 ctrl + /
+            Log.d("문법연습",friendName)
+
+        }
+
     }
 }
