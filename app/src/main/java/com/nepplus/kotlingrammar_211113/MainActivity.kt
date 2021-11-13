28e56dcc3b861ab0b1b8aa85440b7eb483343c7d
구영모 <dudah754@naver.com>
2021-11-13 12:41:22 +0900

    조건문 연습 2 - NOT 연산자 예시

--- app/src/main/java/com/nepplus/kotlingrammar_211113/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlingrammar_211113/MainActivity.kt
@@ -63,5 +63,39 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnCondition02.setOnClickListener {
+
+//            논리 연산자 체험
+
+//            고연봉, 가까운 거리, 야근없음
+
+//            A 회사의 조건
+            val companyASalarly = 3000
+            val companyADistance = 50
+            val companyAWorkEasy = true
+
+//            1번 구직자는 취업 의사?
+//            조건 : 연봉 4천 이상이면 OK
+            val person01OK = companyASalarly >= 4000
+
+            Log.d("조건문02",person01OK.toString())
+
+//            2번 구직자
+//            조건 : 이동시간이 30분 이내 or 빨리 퇴근
+
+            val person02OK = (companyADistance <=30) || (companyAWorkEasy)
+
+//            Bool변수 = "true"는 할 필요가 없는 질문.
+//            Bool 변수 안에 있는 값을 그대로 꺼내쓰면, == true와 항상 같은 결과.
+
+            Log.d("조건문02", person02OK.toString())
+
+//            3번 구직자
+//            조건 : 빨리 퇴근안하는 회사 -> NOT 연산
+
+            val person03ok = !companyAWorkEasy
+            Log.d("조건문02", person03ok.toString())
+        }
+
     }
 }
